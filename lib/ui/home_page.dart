9233f6fb16c16c33d0805b89b4f8193ed66f255f
Juri Alexander <juri1993@hotmail.de>
2024-04-10 12:15:46 +0200

    Create home page with basic controls

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        hintText: 'First or full name',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  IconButton.filled(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+}
